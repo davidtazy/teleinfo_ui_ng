c83f60185d7df00c6bec28828c98985bd06fcc5a
davidtazy <david.allemant@gmail.com>
2023-03-23 19:54:37 +0000

    run && test ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('teleinfo-ui-ng app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('teleinfo-ui-ng app is running!');
   });
 });

1e5fcf3593cc97d9b374b94b5bf965eef69e7ef3
davidtazy <david.allemant@gmail.com>
2023-03-23 22:36:32 +0000

    update app component to show card and debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+import { InfluxService } from './influx.service';
+import { Teleinfo } from './teleinfo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+  papp$: Observable<string | null>
+
+  constructor(private influx: InfluxService) {
+
+    this.papp$ = influx.stream$.pipe(
+      map(
+        samples => {
+          const teleinfo = new Teleinfo(samples, [])
+          return teleinfo.getInstantPower().renderTokW()
+        }
+      )
+    )
+
+  }
+
   title = 'teleinfo-ui-ng';
+
+  papp = "12.3"
+
 }

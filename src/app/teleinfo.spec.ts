0f1e5940a2b5e25954d18ce02a453b6d344eac7f
davidtazy <david.allemant@gmail.com>
2023-03-23 22:33:46 +0000

    create teleinfo  calculator

---
+++ src/app/teleinfo.spec.ts
@@ -0,0 +1,226 @@
+import { Energy, Power, Sample, Teleinfo } from "./teleinfo";
+
+const samples: Sample[] = [
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHCJB",
+    value: "12242517",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHCJR",
+    value: "000000000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHCJW",
+    value: "000014409",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHPJB",
+    value: "18520785",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHPJR",
+    value: "000000000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHPJW",
+    value: "000011398",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "DEMAIN",
+    value: "----",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "HHPHC",
+    value: "A",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "IINST",
+    value: "2",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "IMAX",
+    value: "90",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "ISOUSC",
+    value: "45",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "MOTDETAT",
+    value: "000000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "OPTARIF",
+    value: "BBR(",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "PAPP",
+    value: "570",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "PTEC",
+    value: "HPJB",
+  },
+];
+const zero: Sample[] = [
+  {
+    date: "2022-11-01T10:57:50Z",
+    name: "BBRHPJB",
+    value: "18520000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHPJR",
+    value: "000000000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHPJW",
+    value: "000011398",
+  },
+  {
+    date: "2022-11-01T10:57:50Z",
+    name: "BBRHCJB",
+    value: "12240000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHCJR",
+    value: "000000000",
+  },
+  {
+    date: "2022-11-01T20:57:50Z",
+    name: "BBRHCJW",
+    value: "000014409",
+  },
+];
+
+
+describe('Teleinfo core', () => {
+
+  it("calc instant power", () => {
+    const tt = new Teleinfo(samples, zero);
+
+    const pow = tt.getInstantPower();
+    expect(pow).toEqual(Power.Watt(570));
+
+    expect(pow.renderTokW()).toEqual("0.6");
+  });
+
+  it("calc daily consumtion", () => {
+    const tt = new Teleinfo(samples, zero);
+
+    const cons = tt.getDailyConsumption();
+    expect(cons).toEqual(Energy.WattHour(785));
+
+    expect(cons.renderTokWh()).toEqual("0.8");
+  });
+
+  it("calc instant power when no data", () => {
+    const tt = new Teleinfo([], []);
+
+    const pow = tt.getInstantPower();
+    expect(pow).toEqual(Power.Watt(0));
+
+    expect(pow.renderTokW()).toEqual("0.0");
+  });
+
+  it("calc daily consumtion when no data", () => {
+    const tt = new Teleinfo([], []);
+
+    const cons = tt.getDailyConsumption();
+    expect(cons).toEqual(Energy.WattHour(0));
+
+    expect(cons.renderTokWh()).toEqual("0.0");
+  });
+
+  it("calc nightly consumtion", () => {
+    const tt = new Teleinfo(samples, zero);
+
+    const cons = tt.getNightlyConsumption();
+    expect(cons).toEqual(Energy.WattHour(2517));
+
+    expect(cons.renderTokWh()).toEqual("2.5");
+  });
+
+  it("not red period on Blue period", () => {
+    const tt = new Teleinfo(samples, []);
+
+    expect(tt.isRedPeriod()).toBeFalsy();
+  });
+
+  it("not red period when no data", () => {
+    const tt = new Teleinfo([], []);
+
+    expect(tt.isRedPeriod()).toBeFalsy();
+  });
+
+  it("red period on red period", () => {
+    const tt = new Teleinfo(
+      [
+        {
+          date: "2022-11-01T20:57:50Z",
+          name: "PTEC",
+          value: "HPJR",
+        },
+        {
+          date: "2022-11-01T20:57:50Z",
+          name: "DEMAIN",
+          value: "BLAN",
+        },
+      ],
+      []
+    );
+
+    expect(tt.isRedPeriod()).toBeTruthy();
+    expect(tt.isWhitePeriod()).toBeFalsy();
+  });
+
+  it("red period on demain", () => {
+    const tt = new Teleinfo(
+      [
+        {
+          date: "2022-11-01T20:57:50Z",
+          name: "DEMAIN",
+          value: "ROUG",
+        },
+      ],
+      []
+    );
+
+    expect(tt.isRedPeriod()).toBeTruthy();
+    expect(tt.isWhitePeriod()).toBeFalsy();
+  });
+
+  it("white period on demain", () => {
+    const tt = new Teleinfo(
+      [
+        {
+          date: "2022-11-01T20:57:50Z",
+          name: "DEMAIN",
+          value: "BLAN",
+        },
+      ],
+      []
+    );
+
+    expect(tt.isRedPeriod()).toBeFalsy();
+    expect(tt.isWhitePeriod()).toBeTruthy();
+  });
+
+})
---
+++ src/app/teleinfo.ts
@@ -0,0 +1,126 @@
+export type Sample = {
+    date: string;
+    name: string;
+    value: string;
+};
+
+class Power {
+    readonly watts: number;
+
+    private constructor(val_in_watts: number | string) {
+        this.watts = Number(val_in_watts);
+    }
+
+    renderTokW(): string {
+        return (this.watts / 1000.0).toFixed(1);
+    }
+
+    static Watt(val_in_watts: number | string) {
+        return new Power(val_in_watts);
+    }
+}
+
+class Energy {
+    readonly watt_hour: number;
+
+    private constructor(val_in_watts_hour: number | string) {
+        this.watt_hour = Number(val_in_watts_hour);
+    }
+
+    renderTokWh(): string {
+        return (this.watt_hour / 1000.0).toFixed(1);
+    }
+
+    static WattHour(val_in_watts_hour: number | string) {
+        return new Energy(val_in_watts_hour);
+    }
+}
+
+class Teleinfo {
+    samples: Sample[];
+    zero: Sample[];
+
+    constructor(samples: Sample[], zero: Sample[]) {
+        this.samples = samples;
+        this.zero = zero;
+    }
+
+    getInstantPower(): Power {
+        return Power.Watt(getIntValue("PAPP", this.samples));
+    }
+
+    getDailyConsumption() {
+        return this.getAccuConsumption("BBRHP");
+    }
+
+    getNightlyConsumption() {
+        return this.getAccuConsumption("BBRHC");
+    }
+
+    isRedPeriod() {
+        const ptec = this.find("PTEC");
+        if (ptec && ptec.value.endsWith("JR")) {
+            return true;
+        }
+
+        const demain = this.find("DEMAIN");
+        if (demain === undefined) {
+            return false;
+        }
+        return demain.value.startsWith("ROUG");
+    }
+
+    isWhitePeriod() {
+        if (this.isRedPeriod()) {
+            return false;
+        }
+
+        const demain = this.find("DEMAIN");
+        if (demain === undefined) {
+            return false;
+        }
+        return demain.value.startsWith("BLAN");
+    }
+
+    private getAccuConsumption(key: string) {
+        return this.samples
+            .filter((sample) => sample.name.startsWith(key))
+            .map((sample) => this.getConsumption(sample.name))
+            .reduce(
+                (prev, current) => Energy.WattHour(prev.watt_hour + current.watt_hour),
+                Energy.WattHour(0)
+            );
+    }
+
+    private getConsumption(key: string) {
+        const hp = getIntValue(key, this.samples);
+        const hp_offset = getIntValue(key, this.zero);
+
+        return Energy.WattHour(hp - hp_offset);
+    }
+
+    private find(name: string): Sample | undefined {
+        return this.samples.find((val) => val.name === name);
+    }
+}
+
+
+function getIntValue(name: string, vals: Sample[]): number {
+    const ret = vals
+        .filter((val: Sample) => {
+            return val.name === name;
+        })
+        .map((val: Sample) => {
+            return parseInt(val.value);
+        });
+
+    if (ret.length)
+        return ret.reduce((val: number) => {
+            return val;
+        });
+
+    return 0;
+}
+
+
+export { Teleinfo, Power, Energy };

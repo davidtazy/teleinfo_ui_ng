4f1570daf4ff739d8175a65a9a49f91faa5c6a25
davidtazy <david.allemant@gmail.com>
2023-03-25 11:01:59 +0000

    complete add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { ViewportScroller } from '@angular/common';
+import { SafeCall } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { map, Observable, of } from 'rxjs';
+import { interval, map, Observable, of, sample } from 'rxjs';
 import { InfluxService } from './influx.service';
-import { Teleinfo } from './teleinfo';
+import { Sample, Teleinfo } from './teleinfo';
 
 @Component({
   selector: 'app-root',
@@ -11,22 +13,71 @@ import { Teleinfo } from './teleinfo';
 export class AppComponent {
 
   papp$: Observable<string | null>
+  hc$: Observable<string | null>
+  hp$: Observable<string | null>
+  papp_color: string = "purple"
+  scroll_position: number = 0
 
-  constructor(private influx: InfluxService) {
+  offset: Sample[] = [];
+
+  constructor(private influx: InfluxService, private scroller: ViewportScroller) {
+
+    influx.offset$.subscribe(
+      samples => this.offset = samples
+    )
 
     this.papp$ = influx.stream$.pipe(
       map(
         samples => {
           const teleinfo = new Teleinfo(samples, [])
-          return teleinfo.getInstantPower().renderTokW()
+          const papp = teleinfo.getInstantPower()
+          const percent = Math.min(1, papp.watts / 3000);
+          this.papp_color = this.getColor(percent);
+          return papp.renderTokW()
         }
       )
     )
+    this.hc$ = influx.stream$.pipe(
+      map(
+        samples => {
+          const teleinfo = new Teleinfo(samples, this.offset)
+
+          return teleinfo.getNightlyConsumption().renderTokWh()
+        }
+      )
+    )
+
+    this.hp$ = influx.stream$.pipe(
+      map(
+        samples => {
+          const teleinfo = new Teleinfo(samples, this.offset)
+          return teleinfo.getDailyConsumption().renderTokWh()
+        }
+      )
+    )
+    /*
+        interval(5000).subscribe(_ => {
+          this.scroll_position++;
+          this.scroll_position %= 4
+          this.scroller.scrollToAnchor(this.scroll_position.toString())
+        })
+        */
+
+
 
   }
 
   title = 'teleinfo-ui-ng';
 
-  papp = "12.3"
+  getClock() {
+    const date = new Date();
+    return `${date.getHours()}:${("0" + date.getMinutes()).slice(-2)}`;
+  }
+
+  getColor(value: number): string {
+    //value from 0 to 1
+    var hue = ((1 - value) * 120).toString(10);
+    return ["hsl(", hue, ",100%,50%)"].join("");
+  }
 
 }
